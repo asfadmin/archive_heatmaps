de46d4daca76fd506cb95977bc6be9bbed2780ac
Elliott Lewandowski <erlewandowski@alaska.edu>
2024-05-22 11:28:31 -0800

    Updated SQL command to return the center of each data points lon,lat, updated data processing to use the new center data

--- heatmap.py
+++ heatmap.py
@@ -1,7 +1,9 @@
 import psycopg2
+import re
 from connect_db import connect_to_db
 import matplotlib.pyplot as plt
 from numpy import random
+
 
 def generate_heatmap():
     
@@ -11,8 +13,9 @@ def generate_heatmap():
     #Create a cursor to execute an SQL command
     with conn.cursor() as curs:
         
-        #SQL command to execute, currently hardcoded, should make this a passed parameter in later builds
-        SQL = """SELECT g.granule_name, ST_AsText(shape), g.*
+        #SQL command to execute, currently hardcoded, should make this a passed parameter in later builds        
+        SQL = """
+                SELECT g.granule_name, ST_AsText(ST_Centroid(shape)), g.*
         
                 FROM granule g 
                 
@@ -31,9 +34,27 @@ def generate_heatmap():
         curs.execute(SQL)
         data = curs.fetchall()
         
-        
         #Seperate gathered data into geographic categories
-        location = break_data_on_geo(data)
+        cent = parse_center(data)
+        f = open("parsed_output.txt","w")
+        f.write(str(cent))
+        f.close()
+        
+        location = break_data_on_geo(cent)
+        f = open("loc_ouput.txt","w")
+        f.write(str(location))
+        f.close()
+        
+        
+        ###########
+        #  DEBUG  #
+        ###########
+        
+        f = open("output.txt", "w")
+        for lat in location.keys():
+            for lon in location[lat].keys():
+                f.write("Lat: " + str(lat) + "  Lon: " + str(lon) + "    Value: " + str(location[lat][lon]) + "\n")
+        f.close()
         
         #Plot the resulting data pairs
         x = []
@@ -41,40 +62,67 @@ def generate_heatmap():
         w = []
         
         #Sort the contents of location into x, y positions and corresponding weights
-        for lon in location.keys():
-            for lat in location[lon].keys():
+        for lat in location.keys():
+            for lon in location[lat].keys():
                 x.append(lon)
                 y.append(lat)
-                w.append(location[lon][lat])
+                w.append(location[lat][lon])
         
         #Create a 2D Histogram of the data and export it as png
-        plt.hist2d(x,y,[360,180],weights=w)
+        plt.hist2d(x,y,[180,360],weights=w)
         plt.axis('off')
-        plt.savefig("heatmap.png",bbox_inches='tight', pad_inches=0)
-        
+        plt.savefig("heatmap.png",bbox_inches='tight', pad_inches=0)        
         return
             
-#Create a 2-dimensional dictionary, outer dict is longitude, inner dict is latitude,
-#value is the number of samples data contains within the longitude and latitude pair       
+#Create a 2-dimensional dictionary, outer dict is latitude, inner dict is longitude,
+#value is the number of samples data contains within the latitude and longitude pair
+#
+#Takes a list of tuples with latitude, longitude pairs       
 def break_data_on_geo(data):
 
-    loc = { i : {j : 0 for j in range(-90,91)} for i in range(-180,181) }
-    close_longitude = 0
+    loc = { i : {j : 0 for j in range(-180,181)} for i in range(-90,91) }
     close_latitude = 0
+    close_longitude = 0
     
     for ele in data:
-        #Finds closest longitude
-        for longitude in loc:
-            if abs(longitude - ele[12]) < abs(close_longitude - ele[12]):
-                close_longitude = longitude
-                
-        #Finds closest latitude
-        for latitude in loc[close_longitude]:
-            if abs(latitude - ele[11]) < abs(close_latitude - ele[11]):
+        #Convert current elements lat,lon cords to floats
+        curr_lat = float(ele[1])
+        curr_lon = float(ele[0])
+        #print("Lat: " + str(curr_lat) + "Long: " + str(curr_lon))
+        
+          #Finds closest latitude
+        for latitude in loc.keys():
+            if abs(latitude - curr_lat) < abs(close_latitude - curr_lat):
                 close_latitude = latitude
                 
-        loc[close_longitude][close_latitude] += 1
+        #Finds closest longitude
+        for longitude in loc[close_latitude].keys():
+            if abs(longitude - curr_lon) < abs(close_longitude - curr_lon):
+                close_longitude = longitude
+        
+       
+        loc[close_latitude][close_longitude] += 1
 
     return loc
+
+#Parses the center lat,lon of the passed data, returns a list of tuples
+#Returns a list of long,lat pairs
+def parse_center(data):
+    center = []
+    
+    for ele in data:
+        
+        location = re.findall("-?\d+\.\d+", ele[1])
+        
+        center.append(location)
+    
+    f = open("loc_output.txt","w")
+    for pair in center:
+        f.write(str(pair) + "\n")
+        if abs(float(pair[1])) > 90:
+            print(str(pair))
+    f.close()
+    
+    return center
 
 generate_heatmap()
