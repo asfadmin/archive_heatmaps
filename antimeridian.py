6b37821fab3b53e68a75c8639e3aa339c8576a32
Elliott Lewandowski <erlewandowski@alaska.edu>
2024-05-23 13:40:12 -0800

    Added comments to the check_antimeridian function

--- antimeridian.py
+++ antimeridian.py
@@ -1,12 +1,17 @@
 import shapefile
 
+#Loops over a polygon and checks if it crosses the antimeridian
+#
 def check_antimeridian(poly: list):
     
     for curr_coord in poly:
+        
+        #Pop halves the work if the polygon does not cross the antimeridian
         poly.pop(0)
+        
         for check_coord in poly:
+            
             if abs(curr_coord[0] - check_coord[0]) >= 300:
                 return True
-        
     
     return False
