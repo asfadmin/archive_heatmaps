058d5b539990c157f261019fdebc9f0ab2d83095
Elliott Lewandowski <erlewandowski@alaska.edu>
2024-05-24 12:32:41 -0800

    Polygons are correctly split at the antimeridian, code still needs tidied up

--- antimeridian.py
+++ antimeridian.py
@@ -1,11 +1,11 @@
-import shapefile
 import matplotlib.pyplot as plt
+
 
 #Loops over a polygon and checks if it crosses the antimeridian
 def check_antimeridian(poly: list):
     
     for curr_vertex in poly:
-        #Pop halves the work if the polygon does not cross the antimeridian
+        #Pop here halves the work if the polygon does not cross the antimeridian
         poly.pop(0)
         
         for check_vertex in poly:
@@ -17,7 +17,7 @@ def check_antimeridian(poly: list):
 
 #Split a polygon into two polygons on the antimeridian
 def split_polygon(poly: list):
-    
+
     west_poly = []
     east_poly = []
     
@@ -25,83 +25,97 @@ def split_polygon(poly: list):
     # Create two polygons on either side of the map #
     #################################################
     
-    for vertex in list(poly):
+    for vertex in poly:
         
-        vertex_list = list(vertex)
-        
-        if vertex_list[0] > 0:
+        if vertex[0] > 0:
             east_poly.append(vertex)
             
             #Place vertex in corresponding position on the other side of the prime meridian in the west polygon
-            vertex_list[0] = vertex_list[0] - 360
-            west_poly.append(vertex_list)
+            reflected = [vertex[0] - 360, vertex[1]]
+            west_poly.append(reflected)
             
         else:
             west_poly.append(vertex)
             
             #Place 
-            vertex_list[0] = vertex[0] + 360
-            east_poly.append(vertex_list)      
+            reflected = [vertex[0] + 360, vertex[1]]
+            east_poly.append(reflected)      
     
+    all_poly = [west_poly, east_poly]
     
     ###########################################
     # Create new vertexes on the antimeridian #
     ###########################################
     
-    i = 0
-    while i < len(west_poly) - 1:
+    for dir_poly in all_poly:
         
-        if not (((west_poly[i][0] < -180) and (west_poly[i+1][0] < -180)) or ((west_poly[i][0] > -180) and (west_poly[i+1][0] > -180))):
+        i = 0
+        while i < len(dir_poly) - 1:
             
-            #m=(y1-y2)/(x1-x2)
-            slope = (west_poly[i][1] - west_poly[i+1][1])/(west_poly[i][0] - west_poly[i+1][0])
-            #Distance between our point and the antimeridian
-            distance = (-180) - west_poly[i][0]
-            #The vertical shift required to translate our vertex onto the antimeridian
-            shift = slope * distance
-            
-            #Form and insert a new vertex
-            new_vertex = [-180, west_poly[i][1] + shift]
-            west_poly.insert(i+1, new_vertex)
-            
-            #Skip the entry we just inserted
-            i += 2
-            
-        else:
-            i += 1
-            
-            
-    #############################################
-    # Remove all vertexes past the antimeridian #
-    #############################################
+            if edge_crosses_antimeridian(dir_poly[i],dir_poly[i+1]):
 
-    pop_west = []
-    
-    #Find indexes of all entries to remove
-    for i in range(len(west_poly)):
-        if west_poly[i][0] < -180:
-            pop_west.append(i)
-    
-    #Remove entries in reverse order
-    pop_west.sort(reverse=True)
-    for i in pop_west:
-        west_poly.pop(i)
-    
-    #Ensure that the polygon is still closed
-    if west_poly[0] != west_poly[-1]:
-        west_poly.append(west_poly[0])
-            
+                #m=(y1-y2)/(x1-x2)
+                slope = (dir_poly[i][1] - dir_poly[i+1][1])/(dir_poly[i][0] - dir_poly[i+1][0])
+                
+                #Distance between our point and the antimeridian      
+                distance = 180 - abs(dir_poly[i][0])
+                
+                #Accounts for the negative values being flipped by the abs()
+                if dir_poly[i][0] < 0:
+                    distance *= -1
+                    
+                #The vertical shift required to translate our vertex onto the antimeridian
+                shift = slope * distance
+                
+                #Form and insert a new vertex
+                if dir_poly[i][0] > 0:
+                    new_vertex = [180, dir_poly[i][1] + shift]
+                else:
+                    new_vertex = [-180, dir_poly[i][1] + shift]
+                
+                dir_poly.insert(i+1, new_vertex)#DEBUG
+                
+                #Skip the entry we just inserted
+                i += 2
+                
+            else:
+                i += 1      
+                
+        #############################################
+        # Remove all vertexes past the antimeridian #
+        #############################################
 
+        pop_dir = []
         
-    x_w = [ i[0] for i in west_poly]
-    y_w = [ i[1] for i in west_poly]
-    
-    x_e = [ i[0] for i in east_poly]
-    y_e = [ i[1] for i in east_poly]
-    
-    plt.plot(x_w,y_w)
-    #plt.plot(x_e,y_e,color="red")
+        #Find indexes of all entries to remove
+        for i in range(len(dir_poly)):
+            if abs(dir_poly[i][0]) > 180:
+                pop_dir.append(i)
+        
+        #Remove entries in reverse order
+        pop_dir.sort(reverse=True)
+        for i in pop_dir:
+            dir_poly.pop(i)
+        
+        #Ensure that the polygon is still closed
+        if dir_poly[0] != dir_poly[-1]:
+            dir_poly.append(dir_poly[0])
+            
+    for dir_poly in all_poly:
+        x = [ i[0] for i in dir_poly]
+        y = [ i[1] for i in dir_poly]
+        plt.plot(x,y)
     
     return
 
-
+#Checks if a line between two points would cross the antimeridian
+def edge_crosses_antimeridian(vertex1, vertex2):
+    
+    p1 = abs(vertex1[0])
+    p2 = abs(vertex2[0])
+    
+    #If the two points are not on the same said of the antimeridian return true
+    if not (((p1 > 180) and (p2 > 180)) or ((p1 < 180) and (p2 < 180))):
+        return True
+    
+    return False
