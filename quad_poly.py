fdc52bca640d516ff48f161d007b049fe8827de4
Elliott Lewandowski <erlewandowski@alaska.edu>
2024-06-04 12:51:14 -0800

    Brought in local code; Added ChildNode and start of logic for merging, contains testing code

--- quad_poly.py
+++ quad_poly.py
@@ -3,6 +3,18 @@ import shapely.geometry
 import geopandas as gpd
 import numpy as np
 import traceback
+from math import dist
+
+#Each child has a polygon and a count for how many images it represents
+class ChildNode:
+    def __init__(self,poly: shapely.geometry.Polygon, count = 1):
+        self.poly = poly
+        self.count = count
+        
+    def print(self) -> str:
+        out = str(self.poly) + "\t" + str(self.count)
+        return out
+    
 
 class QuadTree:
     def __init__(self, topL: list, xsize, ysize, children: list):
@@ -10,6 +22,7 @@ class QuadTree:
         self.xsize = xsize
         self.ysize = ysize
         self.children = children
+        self.mergable = 0
         
     def add_child(self, child: shapely.geometry.Polygon):
         self.children.append(child)
@@ -42,14 +55,14 @@ class QuadTree:
     
     #Recursivley split the current quad until the minimum size is reached or each quad contains only 1 child    
     def split(self):
-         # Stops recurssion
+        # Recursivley split data until each quadrant contains one child or has reached minimum size
         if self.ysize > 0.1 and self.xsize > 0.1 and len(self.children) > 1:
             quads = self.quarter()
             
             # Categorize current children
             for child in self.children:
                 for quad in quads:
-                    if quad.contains(child):
+                    if quad.contains(child.poly):
                         quad.add_child(child)
             
             # Store a reference to the contained quads
@@ -58,16 +71,31 @@ class QuadTree:
             # Split each child quadrant
             for child in self.children:
                     child.split()
-        
-        return self
-      
-    #Graph the parent quad and all of its children 
+                    
+        #If we reach minimum size and have more than one child check those children for merging            
+        elif len(self.children) > 1:
+            #Copy children so we can modify the data structure without effecting the QuadTree Node
+            children_copy = self.children.copy()
+            
+            while 0 < len(children_copy):
+                
+                #Remove one child from the copied list and normalize it
+                child = children_copy.pop(0).poly.normalize()
 
-    def plot(self,ax = None):
+                for other in children_copy:
+                    #Check if the two polygons are equal within a tolerance
+                    if child.equals_exact(other.poly.normalize(),1):
+                        pass
+                        
+        
+        return self           
+      
+    #Graph the parent quad and all of its children    
+    def plot(self):
         if any(isinstance(child, QuadTree) for child in self.children):
             for child in self.children:
                 try:
-                    child.plot(ax=ax)
+                    child.plot()
                 except:
                     traceback.print_exception()
         else:
@@ -86,11 +114,8 @@ class QuadTree:
             x.append(x[0])
             y.append(y[0])
             
-            if isinstance(ax, plt.Axes):
-                ax.plot(x,y,color = "black")
-            else:
-                plt.plot(x,y,color = "black")
             
+            plt.plot(x,y,color = "black")
     
     #Debug Function; Print the parent quad and all of its children        
     def print(self):
@@ -98,4 +123,62 @@ class QuadTree:
             for child in self.children:
                 child.print()
         else:
-            print("Coords: (" + str(self.topLeft[0]) + ", " + str(self.topLeft[1]) + ")\tX Size: " + str(self.xsize) + "\tChildren: " + str(len(self.children)) + "\n")
+            child_string = ""
+            for youngster in self.children:
+                child_string += youngster.print() + "\t"
+            if child_string == "":
+                child_string = "None"
+            print("Coords: (" + str(self.topLeft[0]) + ", " + str(self.topLeft[1]) + ")\tX Size: " + str(self.xsize) + "\tChildren: " + child_string + "\n")
+    
+    #Debug Function; Count total number of children a QuadTree contains
+    def count_children(self):
+        sum = 0
+        
+        if any(isinstance(child,QuadTree) for child in self.children):
+            for child in self.children:
+                sum += child.count_children()
+        else:
+            sum = len(self.children)
+        
+        return sum
+    
+            
+
+
+
+
+
+        
+#data = gpd.read_file("./Resources/sat_data.shp")
+
+rng = np.random.default_rng()
+children = [ChildNode(shapely.geometry.Polygon([[1,1],[1,4],[4,4],[4,1],[1,1]])),ChildNode(shapely.geometry.Polygon([[1.2,1.2], [3.8,1.2], [3.8,3.8], [1.2,3.8], [1.2,1.2]])),ChildNode(shapely.geometry.box(0,0,1,1))]
+
+#polys = data["geometry"]
+
+#for i in range(101):
+#    x = rng.random()
+#    y = rng.random()
+#    polys.append(shapely.geometry.box(x,y,x+0.1,y+0.1))
+
+tree = QuadTree([0,5.5], 5.5, 5.5, children=children)
+#tree = QuadTree([-180,90], 360, 180, polys)
+print("Splitting...")
+tree.split()
+
+
+print("Plotting...")
+for child in children:   
+    x,y = child.poly.exterior.xy
+    plt.plot(x,y,alpha=0.6)
+    
+    x,y = child.poly.centroid.xy
+    plt.scatter(x,y)
+
+print("Plotting...")    
+tree.plot()
+
+tree.print()
+
+
+plt.show()
