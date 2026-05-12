5e12bf514e972f68bdb12ac26323ada76cd4438a
Elliott R. Lewandowski <erlewandowski@alaska.edu>
2026-05-12 14:41:37 -0800

    Exporting to a parquet file now!

--- data-ingest/ingest.py
+++ data-ingest/ingest.py
@@ -116,12 +116,12 @@ def ingest_data():
     # Convert merged data to a geojson string
     out_dict = merger.to_dict()
     out_gdf = gpd.GeoDataFrame(out_dict, crs=data_gdf.crs)
-    output = out_gdf.to_json()
+    out_gdf.to_parquet("sat_data.parquet")
 
     # Write the geojson string to a file
-    file = open("sat_data.geojson", "w")
-    file.write(output)
-    file.close()
+    #file = open("sat_data.geojson", "w")
+    #file.write(output)
+    #file.close()
 
     # Clean up the resources folder
     os.system("rm -rf ./Resources")
