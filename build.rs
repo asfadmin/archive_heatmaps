29bd64711a0bdddef1945344d474ca607fee90fb
Elliott R. Lewandowski <erlewandowski@alaska.edu>
2026-06-23 17:27:26 -0800

    Working on getting AsyncDuckDBConnection out of JS and into Rust

--- build.rs
+++ build.rs
@@ -4,7 +4,6 @@ use std::fs;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo::rerun-if-changed=build.rs");
-    println!("cargo::rerun-if-changed=assets/duckdb-browser.mjs");
 
     let res = reqwest::get("https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.33.1-dev57.0/dist/duckdb-browser.mjs").await?;
     let duckdb_browser_mjs = res.text().await?;
--- src/ingest/async_duckdb.rs
+++ src/ingest/async_duckdb.rs
@@ -3,18 +3,23 @@ use leptos::logging::log;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::JsFuture;
 
-#[wasm_bindgen(module = "https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm/dist/")]
+#[wasm_bindgen]
 extern "C" {
+    #[wasm_bindgen(js_name = "AsyncDuckDB")]
+    pub type JsAsyncDuckDB;
 
+    #[wasm_bindgen(js_name="AsyncDuckDBConnection")]
+    #[derive(Debug)]
+    pub type JsAsyncDuckDBConnection;
 }
+
 
 pub async fn download_duckdb_wasm() -> Result<(), JsValue> {
     log!("Preparing DuckDB-wasm module...");
-    const DUCKDB_WASM_URL_BASE: &str = "https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm/dist/";
 
     let js_source_text = "
     (async () => {
-        const duckdb = await import('https://cdn.jsdelivr.net/npm/@duckdb/duckdb-wasm@1.33.1-dev57.0/dist/duckdb-browser.mjs');
+        const duckdb = await import('./duckdb-browser.mjs');
 
         const JSDELIVR_BUNDLES = duckdb.getJsDelivrBundles();
 
@@ -34,32 +39,14 @@ pub async fn download_duckdb_wasm() -> Result<(), JsValue> {
         
         const conn = await db.connect();
 
+
         return conn
     })()
     ";
 
-    let conn: Promise = js_sys::eval(js_source_text)?.dyn_into()?;
-    let conn_await = conn.await?;
+    let conn: Promise = js_sys::eval(js_source_text)?.dyn_into().unwrap();
+    log!("Got promise back from eval");
+    let conn_await: JsAsyncDuckDBConnection = conn.await?.dyn_into().unwrap();
     log!("Conn in rust: {conn_await:?}");
-
-    log!("Evaled js_source_text");
-
-    // // https://duckdb.org/docs/current/clients/wasm/deploying_duckdb_wasm
-    // // Main Library Component
-    // let duckdb_browser_mjs = reqwest::get(format!("{DUCKDB_WASM_URL_BASE}duckdb-browser.mjs")).await?.text().await?;
-    // // Worker Component
-    // let duckdb_browser_eh_worker_js = reqwest::get(format!("{DUCKDB_WASM_URL_BASE}duckdb-browser-eh.worker.js")).await?.text().await?;
-    // // Wasm Module
-    // let duckdb_eh_wasm = reqwest::get(format!("{DUCKDB_WASM_URL_BASE}duckdb-eh.wasm")).await?.bytes().await?;
-    // log!("Downloaded duckdb-eh.wasm");
-    // let a = JsFuture::from(WebAssembly::instantiate_buffer(&duckdb_eh_wasm, &Object::new())).await?;
-    // log!("Created JsValue from .wasm");
-    // let b: WebAssembly::Instance = Reflect::get(&a, &"instance".into())?.dyn_into()?;
-    // log!("WebAssembly Instance created");
-    // let c = b.exports();
-    // log!("MJS: {duckdb_browser_mjs:?}");
-    // log!("WASM Instance: {b:?}");
-    // log!("WASM Exports: {c:?}");
-
     Ok(())
 }
