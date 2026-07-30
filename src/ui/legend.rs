4baa6683885a3c80159a8b6e9a88cf5cd033e932
Elliott R. Lewandowski <erlewandowski@alaska.edu>
2026-07-29 17:59:08 -0800

    temp: done for the day

--- src/ui/legend.rs
+++ src/ui/legend.rs
@@ -1,5 +1,6 @@
 use leptos::html::div;
 use leptos::{IntoView, component, prelude::*, view};
+use leptos::logging::log;
 
 use crate::types::ExpansionSignal;
 use crate::ui::expansion_button::ExpansionButton;
@@ -13,7 +14,7 @@ pub fn Legend() -> impl IntoView {
     let MaxWeightSignal(max_weight) = use_context::<MaxWeightSignal>()
         .expect("Failed to get max weight signal in Legend");
 
-    let LEGEND_WEIGHTS: &[u32; 8] = &[
+    let legend_weights: &[u32; 8] = &[
         30,
         80,
         130,
@@ -23,19 +24,21 @@ pub fn Legend() -> impl IntoView {
         370,
         450,
     ];
+    let legend_weights_len = 7;
 
     let colormap_bytes = include_bytes!("../../assets/magma.png");
     let colormap_image = image::load_from_memory(colormap_bytes)
         .expect("ERROR: Failed to generate image from colormap_bytes")
         .to_rgba8();
 
-    let colors: Vec<(usize, String, _)> = LEGEND_WEIGHTS.iter().enumerate().map(|(i, x)| {
-        let weight = move || ((x*max_weight() as u32) as f32)/(480.0*1.32);
+    let colors_vec: Vec<(usize, String, _)> = legend_weights.iter().enumerate().map(|(i, x)| {
+        log!("Color vec {i}");
+        let weight = move || ((x*max_weight() as u32) as f32)/(480.0*1.32)
         let pixel = colormap_image.get_pixel(*x, 0).0;
         (i, format!("background-color: #{:02x}{:02x}{:02x}", pixel[0], pixel[1], pixel[2]), weight)
     }).collect();
 
-    let (sig, set_sig) = signal(colors);
+    let (colors, _) = signal(colors_vec);
 
     view! {
         <div
@@ -46,7 +49,7 @@ pub fn Legend() -> impl IntoView {
             <Show when=move || { expanded() }>
                 <div class="legend-container">
                     <For
-                        each=move || sig()
+                        each=move || colors()
                         key=|x| x.0
                         children=move |x| {
                             view!{
@@ -54,7 +57,14 @@ pub fn Legend() -> impl IntoView {
                                     class="legend-box"
                                     style=x.1
                                 ></div>
-                                <p>{x.2}</p>
+                                <p>{
+                                    log!("Handling: {}", x.0);
+                                    match x.0 {
+                                        0 => {format!("< {}", x.2())}
+                                        legend_weights_len => {format!("> {}", x.2())}
+                                        _ => {format!("{} - {}", x.2(), x.2())}
+                                    }
+                                }</p>
                             }
                         }
                     />
