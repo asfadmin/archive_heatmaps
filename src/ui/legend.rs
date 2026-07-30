c883d3967626742cfebc89103602856342906f84
Elliott R. Lewandowski <erlewandowski@alaska.edu>
2026-07-30 15:37:07 -0800

    feat: legend scales number of entries based on max weight

--- src/ui/legend.rs
+++ src/ui/legend.rs
@@ -27,58 +27,29 @@ pub fn Legend() -> impl IntoView {
         match max {
             i if i <= granularity => (1..i).map(|n| n).collect::<Vec<u32>>(),
             i @ _ => (1..granularity)
-                .filter_map(|n| {
-                    let weight = (i as f32 * ((n as f32) / granularity as f32)) as u32;
-                    let val = calc_tex_coord(weight as f32, i as f32);
-                    log!("Weight: {weight}\tTex Coord: {val}");
-                    if val > 0 && val < 480 {
-                        Some(weight)
-                    } else {
-                        None
-                    }
+                .map(|n| {
+                    (i as f32 * ((n as f32) / granularity as f32)) as u32
                 })
                 .collect::<Vec<u32>>(),
         }
         .iter()
-        .map(|x| match x {
+        .filter_map(|x| {
+            let scaled_weight = match x {
                 n @ 0..10 => *n as usize,
                 n => ((*n as f64 / 5.0).round() * 5.0) as usize,
+            };
+            let val = calc_tex_coord(scaled_weight as f32, max as f32);
+            log!("Weight: {scaled_weight}\tTex Coord: {val}");
+            if val > 0 && val < 480 {
+                Some(scaled_weight)
+            } else {
+                None
             }
-        )
+
+        })
         .enumerate()
         .collect::<Vec<(usize, usize)>>()
     };
-
-
-
-    let colormap_bytes = include_bytes!("../../assets/magma.png");
-    let colormap_image = Arc::new(
-        image::load_from_memory(colormap_bytes)
-            .expect("ERROR: Failed to generate image from colormap_bytes")
-            .to_rgba8(),
-    );
-
-    // let colors = move || {
-    //     let img = colormap_image.clone();
-    //     weights()
-    //         .iter()
-    //         .enumerate()
-    //         .map(|(i, x)| {
-    //             let max = max_weight();
-    //             let coord = calc_tex_coord(*x as f32, max as f32);
-    //             let pixel = colormap_image.clone().get_pixel(coord, 0).0;
-
-    //             (
-    //                 i,
-    //                 format!(
-    //                     "background-color: #{:02x}{:02x}{:02x}",
-    //                     pixel[0], pixel[1], pixel[2]
-    //                 ),
-    //             )
-    //         })
-    //         .collect::<Vec<(usize, String)>>()
-    // };
-    // let (csig, _) = signal(colors());
 
     view! {
         <div
@@ -95,12 +66,27 @@ pub fn Legend() -> impl IntoView {
                     </h3>
                     <For
                         each=move || weights()
-                        key=|x| x.1
+                        key=|x| x.0 + x.1
                         children=move |(i, x)| {
+                            log!("Handling: {}", i);
                             let wvec = weights();
                             log!("wvec: {wvec:?}");
                             let row = format!("{}", i + 2);
-                            log!("Handling: {}", i);
+                            
+                            // Reading the image every time the signal update feels bad...
+                            let colormap_bytes = include_bytes!("../../assets/magma.png");
+                            let colormap_image = image::load_from_memory(colormap_bytes)
+                                .expect("ERROR: Failed to generate image from colormap_bytes")
+                                .to_rgba8();
+
+                            let max = max_weight();
+                            let coord = calc_tex_coord(x as f32, max as f32);
+                            let pixel = colormap_image.get_pixel(coord, 0).0;
+
+                            let background_color = format!(
+                                "background-color: #{:02x}{:02x}{:02x}",
+                                pixel[0], pixel[1], pixel[2]
+                            );
 
                             let legend_text = match i {
                                 0 => {
@@ -110,7 +96,15 @@ pub fn Legend() -> impl IntoView {
                                         format!("{x}")
                                     }
                                 }
-                                i if i+1 == wvec.len() => {format!("> {}", wvec[i-1].1)}
+                                i if i+1 == wvec.len() => {
+                                    let last = wvec[i-1].1;
+                                    if x-last > 1 {
+                                        format!("> {}", wvec[i-1].1)
+                                    } else {
+                                        format!("{x}")
+                                    }
+                                    
+                                }
                                 _ => {
                                     let last = wvec[i-1].1;
                                     if x-last > 1 {
@@ -121,10 +115,12 @@ pub fn Legend() -> impl IntoView {
                                 }
                             };
 
+                            log!("legend_text: {legend_text:?}");
+
                             view!{
                                 <div
                                     class="legend-box"
-                                    // style=x.1
+                                    style=background_color
                                     style:grid-row=row.clone()
                                 ></div>
                                 <span
