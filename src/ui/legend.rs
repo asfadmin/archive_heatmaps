68b4f559f91c77eacf1777778d61fae7e463696f
Elliott R. Lewandowski <erlewandowski@alaska.edu>
2026-07-30 15:04:36 -0800

    dev: dynamic legend text

--- src/ui/legend.rs
+++ src/ui/legend.rs
@@ -1,3 +1,6 @@
+use std::rc::Rc;
+use std::sync::Arc;
+
 use leptos::logging::log;
 use leptos::{IntoView, component, prelude::*, view};
 
@@ -13,71 +16,69 @@ pub fn Legend() -> impl IntoView {
     let MaxWeightSignal(max_weight) =
         use_context::<MaxWeightSignal>().expect("Failed to get max weight signal in Legend");
 
-    let calc_tex_coord = |x: f32, max: f32| {
-        (((x*1.32)/max) * 480.0) as u32
-    };
+    let calc_tex_coord = |x: f32, max: f32| (((x * 1.32) / max) * 480.0) as u32;
 
-
-    // List of evenly distributed weights that can be mapped to tex coords
-    let weight_samples = move || {
+    // List of weights to use for the legend
+    let weights = move || {
+        log!("Updating Weights Signal");
+        let max = max_weight();
+        log!("Max weight in signal: {max}");
         let granularity = 8;
-        match max_weight() {
+        match max {
             i if i <= granularity => (1..i).map(|n| n).collect::<Vec<u32>>(),
             i @ _ => (1..granularity)
                 .filter_map(|n| {
                     let weight = (i as f32 * ((n as f32) / granularity as f32)) as u32;
                     let val = calc_tex_coord(weight as f32, i as f32);
                     log!("Weight: {weight}\tTex Coord: {val}");
-                    if val > 0 && val < 480 { Some(weight) } else { None }
+                    if val > 0 && val < 480 {
+                        Some(weight)
+                    } else {
+                        None
+                    }
                 })
                 .collect::<Vec<u32>>(),
         }
+        .iter()
+        .map(|x| match x {
+                n @ 0..10 => *n as usize,
+                n => ((*n as f64 / 5.0).round() * 5.0) as usize,
+            }
+        )
+        .enumerate()
+        .collect::<Vec<(usize, usize)>>()
     };
 
-    // List of tex coords we can map to weights
-    let legend_weights: &[u32; 8] = &[30, 80, 130, 170, 230, 300, 370, 450];
+
 
     let colormap_bytes = include_bytes!("../../assets/magma.png");
-    let colormap_image = image::load_from_memory(colormap_bytes)
-        .expect("ERROR: Failed to generate image from colormap_bytes")
-        .to_rgba8();
+    let colormap_image = Arc::new(
+        image::load_from_memory(colormap_bytes)
+            .expect("ERROR: Failed to generate image from colormap_bytes")
+            .to_rgba8(),
+    );
 
-    let colors_vec: Vec<(usize, String)> = legend_weights
-        .iter()
-        .enumerate()
-        .map(|(i, x)| {
-            let pixel = colormap_image.get_pixel(*x, 0).0;
+    // let colors = move || {
+    //     let img = colormap_image.clone();
+    //     weights()
+    //         .iter()
+    //         .enumerate()
+    //         .map(|(i, x)| {
+    //             let max = max_weight();
+    //             let coord = calc_tex_coord(*x as f32, max as f32);
+    //             let pixel = colormap_image.clone().get_pixel(coord, 0).0;
 
-            (
-                i,
-                format!(
-                    "background-color: #{:02x}{:02x}{:02x}",
-                    pixel[0], pixel[1], pixel[2]
-                ),
-            )
-        })
-        .collect();
-    let legend_len = colors_vec.len() - 1;
-
-    let weights = move || {
-        let max = max_weight();
-        log!("Weight Samples: {:?}", weight_samples());
-
-        legend_weights
-            .iter()
-            .map(|x| {
-                let val = (((x * max) as f32) / (480.0 * 1.32)) as usize;
-                match val {
-                    n @ 0..10 => n,
-                    n => ((n as f64 / 5.0).round() * 5.0) as usize,
-                }
-            })
-            .collect::<Vec<usize>>()
-    };
-
-    // Feels bad to wrap a derived signal in a signal to avoid type errors...
-    let (weights_sig, _) = signal(weights);
-    let (colors, _) = signal(colors_vec);
+    //             (
+    //                 i,
+    //                 format!(
+    //                     "background-color: #{:02x}{:02x}{:02x}",
+    //                     pixel[0], pixel[1], pixel[2]
+    //                 ),
+    //             )
+    //         })
+    //         .collect::<Vec<(usize, String)>>()
+    // };
+    // let (csig, _) = signal(colors());
 
     view! {
         <div
@@ -93,28 +94,42 @@ pub fn Legend() -> impl IntoView {
                         Acquisitions
                     </h3>
                     <For
-                        each=move || colors()
-                        key=|x| x.0
-                        children=move |x| {
-                            let row = format!("{}", x.0 + 2);
+                        each=move || weights()
+                        key=|x| x.1
+                        children=move |(i, x)| {
+                            let wvec = weights();
+                            log!("wvec: {wvec:?}");
+                            let row = format!("{}", i + 2);
+                            log!("Handling: {}", i);
+
+                            let legend_text = match i {
+                                0 => {
+                                    if x > 1 {
+                                        format!("<= {}", x)
+                                    } else {
+                                        format!("{x}")
+                                    }
+                                }
+                                i if i+1 == wvec.len() => {format!("> {}", wvec[i-1].1)}
+                                _ => {
+                                    let last = wvec[i-1].1;
+                                    if x-last > 1 {
+                                        format!("{} - {}", last+1, x)
+                                    } else {
+                                        format!("{x}")
+                                    }
+                                }
+                            };
+
                             view!{
                                 <div
                                     class="legend-box"
-                                    style=x.1
+                                    // style=x.1
                                     style:grid-row=row.clone()
                                 ></div>
                                 <span
                                     style:grid-row=row
-                                >{ move || {
-                                        let weights_vec = weights_sig()();
-                                        log!("Handling: {}", x.0);
-                                        match x.0 {
-                                            0 => {format!("< {}", weights_vec[x.0])}
-                                            i if i == legend_len => {format!("> {}", weights_vec[x.0])}
-                                            _ => {format!("{} - {}", weights_vec[x.0 - 1], weights_vec[x.0])}
-                                        }
-                                    }
-                                }</span>
+                                >{legend_text}</span>
                             }
                         }
                     />
